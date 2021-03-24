c130f20381e98a59ee1a4b0cfff1ef34c411dc38
Johan Holmberg <johan@idioti.se>
2021-03-24 15:18:44 +0100

    Added an apt update to fetch new repos

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Installs Jest as per https://help.codegrade.com/user-reference/autotest-general/unit-test
 cg-jest install
 
+# Updates the Ubuntu repo settings
+sudo apt update
+
 # Installs Firefox and a headless X window manager
 sudo apt install firefox xvfb
 
